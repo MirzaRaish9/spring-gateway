fe6219d0d6393477a582f9870bd65c970faa9a55
Win <Win@Win-PC>
2022-12-29 01:20:25 +0530

    commit test

---
+++ spring-gateway/src/main/java/com/api/springgateway/SpringGatewayApplication.java
@@ -0,0 +1,18 @@
+package com.api.springgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+
+
+@SpringBootApplication
+
+public class SpringGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGatewayApplication.class, args);
+	}
+
+}
